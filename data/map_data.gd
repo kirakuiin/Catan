867567f9aa8192d32c8d3183b7e27a4f062ae08d
kirakuiin <wang.zhuowei@foxmail.com>
2022-10-17 08:15:43 +0800

    1. 调整数据文件目录

---
+++ data/map_data.gd
@@ -0,0 +1,24 @@
+extends Node
+
+
+class_name MapData
+
+
+const MAP_DATA = {
+    Data.CatanSize.SMALL: {
+        Data.SeafarerMap.NEW_SHORE: {
+            "main": [
+                Vector3(0, 0, 0), Vector3(1, -1, 0), Vector3(-1, 1, 0), Vector3(0, 1, -1),
+                Vector3(0, -1, 1), Vector3(1, 0, -1), Vector3(-1, 0, 1), Vector3(-2, 2, 0),
+                Vector3(2, -2, 0), Vector3(0, 2, -2), Vector3(0, -2, 2), Vector3(2, 0, -2),
+                Vector3(-2, 0, 2), Vector3(2, -1, -1), Vector3(-2, 1, 1), Vector3(-1, 2, -1),
+                Vector3(1, -2, 1), Vector3(-1, -1, 2), Vector3(1, 1, -2),
+            ],
+            "others": {
+            },
+        },
+    },
+    Data.CatanSize.BIG: {
+    },
+}
+

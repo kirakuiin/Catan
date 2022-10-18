b26d6003c9877b865a92c24434c1297747e1d3d5
kirakuiin <wang.zhuowei@foxmail.com>
2022-10-18 08:16:37 +0800

    1. 提交新海岸地图的骨架

--- data/map_data.gd
+++ data/map_data.gd
@@ -7,15 +7,17 @@ class_name MapData
 const MAP_DATA = {
     Data.CatanSize.SMALL: {
         Data.SeafarerMap.NEW_SHORE: {
-            "main": [
-                Vector3(0, 0, 0), Vector3(1, -1, 0), Vector3(-1, 1, 0), Vector3(0, 1, -1),
-                Vector3(0, -1, 1), Vector3(1, 0, -1), Vector3(-1, 0, 1), Vector3(-2, 2, 0),
-                Vector3(2, -2, 0), Vector3(0, 2, -2), Vector3(0, -2, 2), Vector3(2, 0, -2),
-                Vector3(-2, 0, 2), Vector3(2, -1, -1), Vector3(-2, 1, 1), Vector3(-1, 2, -1),
-                Vector3(1, -2, 1), Vector3(-1, -1, 2), Vector3(1, 1, -2),
+            "main": [ 
+                Vector3(-1, 0, 1), Vector3(-1, 1, 0), Vector3(0, 0, 0), Vector3(0, -1, 1), 
+                Vector3(-1, -1, 2), Vector3(-2, 0, 2), Vector3(-2, 1, 1), Vector3(-2, 2, 0), 
+                Vector3(-1, 2, -1), Vector3(0, 1, -1), Vector3(1, 0, -1), Vector3(1, -1, 0),
+                Vector3(1, -2, 1), Vector3(0, -2, 2), Vector3(-1, -2, 3), Vector3(-2, -1, 3),
+                Vector3(-3, 0, 3), Vector3(-3, 1, 2), Vector3(-3, 2, 1),
             ],
-            "others": {
-            },
+            "island": [
+                Vector3(-3, 4, -1), Vector3(0, 3, -3), Vector3(-1, 4, -3), Vector3(3, -3, 0),
+                Vector3(3, -2, -1), Vector3(3, 0, -3), Vector3(3, 1, -4), Vector3(2, 2, -4),
+            ],
         },
     },
     Data.CatanSize.BIG: {
--- game/map/seafarer.gd
+++ game/map/seafarer.gd
@@ -13,4 +13,18 @@ func _init(setup_info: Protocol.CatanSetupInfo):
 
 
 func generate() -> Protocol.MapInfo:
-    return Protocol.MapInfo.new()
+    var map_data = MapData.MAP_DATA[_setup_info.catan_size][_setup_info.expansion_mode.selected_map]
+    var result = Protocol.MapInfo.new()
+    for pos in map_data.main:
+        result.add_tile(Protocol.TileInfo.new(pos))
+    for pos in map_data.island:
+        result.add_tile(Protocol.TileInfo.new(pos))
+    return result
+
+
+func _fill_small_map() -> Array:
+    var center = Vector3(-1, 0, 1)
+    var result = []
+    for hex in Hexlib.get_spiral_ring(Hexlib.create_hex(center), 3):
+        result.append('Vector3%s' % hex.to_vector3())
+    return result
